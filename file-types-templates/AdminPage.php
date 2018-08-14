e7ef03259176e80adcc93c10f4d68a6030ed3ea1
Domenico Giambra <zorinik@gmail.com>
2018-08-14 18:54:59 +0200

    Added file templates

---
+++ file-types-templates/AdminPage.php
@@ -0,0 +1,26 @@
+<?php namespace {namespace};
+
+use Model\Admin\AdminPage;
+
+class {name} extends AdminPage
+{
+	public function options(): array
+	{
+		return [];
+	}
+
+	public function customize()
+	{
+
+	}
+
+	public function viewOptions(): array
+	{
+		return [];
+	}
+
+	public function visualizerOptions(): array
+	{
+		return [];
+	}
+}
