679400b33f6b13cfcb107e9e645dec09a773d25c
Domenico Giambra <zorinik@gmail.com>
2025-01-27 20:23:28 +0100

    Sposto migration a phinx

---
+++ migrations/20000101000000_admin_users.php
@@ -0,0 +1,53 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AdminUsers extends AbstractMigration
+{
+	public function change()
+	{
+		if (!$this->hasTable('admin_users')) {
+			$profiles = $this->table('admin_profiles')
+				->addColumn('name', 'string', ['null' => false])
+				->create();
+
+			$profiles->insert([
+				'id' => 1,
+				'name' => 'admin',
+			])->saveData();
+
+			$users = $this->table('admin_users')
+				->addColumn('username', 'string', ['null' => false])
+				->addColumn('password', 'string', ['null' => false])
+				->addColumn('profile', 'integer', ['null' => true])
+				->addForeignKey('profile', 'admin_profiles', 'id', ['delete' => 'RESTRICT', 'update' => 'CASCADE'])
+				->create();
+
+			$users->insert([
+				'id' => 1,
+				'username' => 'admin',
+				'password' => password_hash('admin', PASSWORD_DEFAULT),
+				'profile' => 1,
+			])->saveData();
+
+			$this->table('admin_privileges')
+				->addColumn('page', 'string', ['null' => true])
+				->addColumn('subpage', 'string', ['null' => true])
+				->addColumn('profile', 'integer', ['null' => true])
+				->addColumn('user', 'integer', ['null' => true])
+				->addColumn('C', 'boolean', ['null' => false, 'default' => true])
+				->addColumn('C_special', 'string', ['null' => true])
+				->addColumn('R', 'boolean', ['null' => false, 'default' => true])
+				->addColumn('R_special', 'string', ['null' => true])
+				->addColumn('U', 'boolean', ['null' => false, 'default' => true])
+				->addColumn('U_special', 'string', ['null' => true])
+				->addColumn('D', 'boolean', ['null' => false, 'default' => true])
+				->addColumn('D_special', 'string', ['null' => true])
+				->addColumn('L', 'boolean', ['null' => false, 'default' => true])
+				->addColumn('L_special', 'string', ['null' => true])
+				->addForeignKey('user', 'admin_users', 'id', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+				->addForeignKey('profile', 'admin_profiles', 'id', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+				->create();
+		}
+	}
+}
